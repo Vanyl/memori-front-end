d110b5a3244d6ed5652b0265f99d04565c0bb5d4
Yavanna Perez <yavannaperez@hotmail.com>
2024-05-23 17:30:36 +0200

    home page with navbar + banner

---
+++ memori/src/views/Home.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Banner from '@/components/Banner.vue'
+
+</script>
+
+<template>
+    <div class="home">
+        <Banner title="MEMORI" />
+        <div class="content">
+        <!-- <h1>home page</h1> -->
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.home {
+    height: 200vh;
+    padding: 0;
+    margin: 0;
+}
+.content{
+    padding-top: 20vh;
+}
+</style>
