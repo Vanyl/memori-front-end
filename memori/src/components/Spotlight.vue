f96ed9f0ce8dbeb4155993e04d6fef25a849f15f
Yavanna Perez <yavannaperez@hotmail.com>
2024-05-23 19:39:52 +0200

    Spotlight component created basic css done

---
+++ memori/src/components/Spotlight.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="spotlight-container">
+<img class="spotlight-img" src="@/assets/images/000002.JPG" alt="">
+<div class="spotlight-info">
+    <h3 class="spotlight-title">title spotlight</h3>
+    <div class="spotlight-description-box"><p>description...</p></div>
+    <p>You don't know what you're looking for ? Here's some explanation and suggestion to help.</p>
+</div>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'Spotlight'
+}
+
+</script>
+
+<style scoped>
+.spotlight-container{
+    margin-top: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 50px;
+}
+
+.spotlight-img{
+    width :335px;
+    height: 200px;
+    object-fit: cover;
+}
+
+.spotlight-info{
+    max-width: 25%
+}
+
+.spotlight-description-box{
+    width: 230px;
+    height: 100px;
+    border: 1px solid black;
+}
+
+.spotlight-description-box p{
+    margin: 5px;
+}
+</style>
