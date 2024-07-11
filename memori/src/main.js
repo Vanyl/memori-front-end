9e7d472e4d761959f8cb1a60614ea38ef353d0d0
Yavanna Perez <yavannaperez@hotmail.com>
2024-07-11 11:33:41 +0200

    vue form installed

--- memori/src/main.js
+++ memori/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from '@/router/index.js'
+import Vueform from '@vueform/vueform'
+import vueformConfig from './../vueform.config'
 
-
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router).mount('#app')
+app.use(Vueform, vueformConfig)
