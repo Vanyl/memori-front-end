a63add9b2b3859b7abb4a51dd649fb6ea964dc92
Yavanna Perez <yavannaperez@hotmail.com>
2024-07-12 17:13:53 +0200

    uninstall vueform

--- memori/src/main.js
+++ memori/src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from '@/router/index.js'
-import Vueform from '@vueform/vueform'
-import vueformConfig from './../vueform.config'
 
 const app = createApp(App)
 app.use(router).mount('#app')
-app.use(Vueform, vueformConfig)
